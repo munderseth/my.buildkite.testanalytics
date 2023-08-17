bcfeda5c13bd4e74928315ed18262521cf123114
munderseth <marku@s2technologies.com>
2023-08-17 15:31:39 -0700

    use echo

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,8 @@
+call echo "Install packages"
 call npm install
+call echo "Run tests"
 call npm test
+call echo "Configure Testspace"
 call testspace config url munderseth.testspace.com
-call testspace junit.xml
+call echo "Publish Results to Testspace"
+call testspace junit.xml{tests}
